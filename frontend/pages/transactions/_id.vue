3c9f9d91e26baaf0d5d2f50bba988b64bafff9b9
Anurag Dalmia <52538845+dalmia007@users.noreply.github.com>
2022-10-03 17:36:29 +0530

    feat: added bank to transaction detail page

--- frontend/pages/transactions/_id.vue
+++ frontend/pages/transactions/_id.vue
@@ -48,6 +48,16 @@
             </dd>
           </div>
           <div class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">Bank</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {{
+                transaction.account
+                  ? transaction.account.bank
+                  : 'Bank Not Specified'
+              }}
+            </dd>
+          </div>
+          <div class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
             <dt class="text-sm font-medium text-gray-500">Amount</dt>
             <dd
               class="mt-1 text-xl text-gray-900 sm:col-span-2 sm:mt-0"

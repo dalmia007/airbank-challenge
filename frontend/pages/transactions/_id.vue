394d39a28125808ee850fd36d678b1050e33c6fc
Anurag Dalmia <52538845+dalmia007@users.noreply.github.com>
2022-10-04 01:48:46 +0530

    feat: added ability to add custom categories

--- frontend/pages/transactions/_id.vue
+++ frontend/pages/transactions/_id.vue
@@ -80,7 +80,7 @@
               <CategoryTag :category="transaction.category" />
               <button
                 type="button"
-                :class="showCategoryChange ? 'bg-red-600' : 'bg-black'"
+                :class="showCategoryChange ? 'bg-red-600' : 'bg-green-600'"
                 class="mt-2 inline-flex items-center rounded-md border border-transparent px-4 py-2 text-sm font-light text-white shadow-sm"
                 @click="
                   showCategoryChange
@@ -90,14 +90,11 @@
               >
                 {{ showCategoryChange ? 'Cancel' : 'Change Category' }}
               </button>
-              <div
-                v-if="showCategoryChange"
-                class="flex flex-col mt-2 w-max-[300px]"
-              >
+              <div v-if="showCategoryChange" class="flex flex-col mt-2 w-80">
                 <select
                   v-model="selectedCategory"
                   name="category"
-                  class="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm text-xs md:text-base"
+                  class="mt-1 block rounded-md border-gray-300 py-2 pl-3 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 text-sm md:text-base"
                   @change="updateCategory"
                 >
                   <option :value="undefined">Select New Category</option>
@@ -109,6 +106,32 @@
                     {{ category.name }}
                   </option>
                 </select>
+                <div class="mt-3">
+                  <label
+                    for="category"
+                    class="block text-sm font-bold text-black"
+                    >OR Create New Category</label
+                  >
+                  <div class="flex justify-between">
+                    <div class="mt-1">
+                      <input
+                        id="category"
+                        v-model="createdCategory"
+                        type="text"
+                        name="category"
+                        class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm py-2"
+                        placeholder="New Category Name"
+                      />
+                    </div>
+                    <button
+                      type="button"
+                      class="inline-flex items-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-sm font-light text-white shadow-sm"
+                      @click="addCategory"
+                    >
+                      Add
+                    </button>
+                  </div>
+                </div>
               </div>
             </dd>
           </div>
@@ -122,6 +145,7 @@
 import getTransactionById from '~/apollo/queries/getTransactionById'
 import getAllCategories from '~/apollo/queries/getAllCategories'
 import updateTransactionCategory from '~/apollo/mutations/updateTransactionCategory'
+import addTransactionCategory from '~/apollo/mutations/addTransactionCategory'
 
 import CategoryTag from '~/components/CategoryTag'
 
@@ -135,6 +159,7 @@ export default {
       transaction: {},
       showCategoryChange: false,
       selectedCategory: undefined,
+      createdCategory: undefined,
     }
   },
   methods: {
@@ -157,6 +182,25 @@ export default {
         .then(() => {
           this.showCategoryChange = false
           this.$apollo.queries.getTransactionById.refetch()
+        })
+        .finally(() => {
+          this.update = false
+        })
+    },
+    addCategory() {
+      this.$apollo
+        .mutate({
+          mutation: addTransactionCategory,
+          variables: {
+            categoryName: this.createdCategory,
+            transactionId: this.transaction.id,
+          },
+        })
+        .then(() => {
+          this.showCategoryChange = false
+          this.createdCategory = ''
+          this.$apollo.queries.getTransactionById.refetch()
+          this.$apollo.queries.getAllCategories.refetch()
         })
         .finally(() => {
           this.update = false
